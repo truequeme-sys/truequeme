d9d6f4525943f11b474f310af9bf714110d87563
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-07-31 00:29:55 -0500

    Mis trueques y publicacion de trueques

--- app/Http/Controllers/TruequeController.php
+++ app/Http/Controllers/TruequeController.php
@@ -61,7 +61,7 @@ class TruequeController extends Controller
     public function getArtefacto(Request $request)
     {
         $artefacto=Artefacto::where("id",$request->input("id"))->first();
-        dd($artefacto);
+        //dd($artefacto);
         return response()->json($artefacto);
     }
 }
