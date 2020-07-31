b2e87b3bc549ed9159390012018b5980c056bc99
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-07-30 23:28:52 -0500

    Mis trueques y publicacion de trueques

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -64,7 +64,7 @@ class PerfilController extends Controller
         $user= Auth::user();
         $artefactos = Artefacto::orderBy('created_at', 'desc');
         $artefactos->where('user_id',$user->id );
-        dd($user->id );
+        dd($artefactos );
        return view('perfil.trueques', compact('artefactos'));
         # code...
     }
